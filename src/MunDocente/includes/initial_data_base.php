b799f7d7893962aadc0c9bcbcbbe9b6e807187eb
Álvaro R. Hernández M <alvaro.hernandez01@uptc.edu.co>
2016-03-12 18:41:36 -0500

    MODELO E-R CON TIPO DE DATOS SCRIPT DE SELECCIÓN  DE BASE DE DATOS Y CREACIÓN DE TABLAS

---
+++ src/MunDocente/includes/initial_data_base.php
@@ -0,0 +1,200 @@
+<?php
+$server_name = 'localhost';
+$user_db = 'munDocente';
+$password_db = 'munDocente';
+$data_base = 'mundocente';
+//CONEXIÓN DE LA BASE DE DATOS
+$connection = mysqli_connect($server_name,$user_db,$password_db);
+//COMPROBAMOS LA CONEXIÓN
+if(!$connection){
+	die('No se ha podido establecer conexión con la base de datos. ' . mysqli_connect_error());
+} else {
+	echo 'Conexión exitosa... <br/>';
+}
+
+//CREACIÓN DE LA BASE DE DATOS **OJO: CREARLA MANUALMENTE CON EL NOMBRE DE mundocente Y COTEJAMIENTO utf8-unicode-ci
+//$sql = 'CREATE or REPLACE DATABASE mundocente ';
+//if( (mysqli_query($connection, $sql))){
+//	echo 'Creación exitoso de la base de datos <br/>';
+//} else {
+//	echo 'Error en la creación de la base de datos' . mysqli_error($connection);
+//}
+
+//SELECIONAMOS LA BASE DE DATOS
+$database = mysqli_select_db($connection, $data_base);
+if(!$database){
+	die('No se ha podido selecionar la base de datos '. $data_base);
+} else {
+	echo 'Seleccionada correctamente la base de datos... <br/>';
+}
+//PROBLEMA SOLUCIONADO DE TILDES Y DEMÁS CARACTERES
+mysqli_query($connection,"SET NAMES 'utf8'");
+//PARA NO TENER PROBLEMAS CON LAS LLAVES FORANEAS
+mysqli_query($connection,"SET_FOREIGN_KEY_CHECKS=0");
+
+
+//CREAMOS LA TABLA TIPO DE INSTITUCIÓN ACADEMICA
+$sql = explode(";", file_get_contents('data_base/create_type_of_academic_institution.sql')); //abrimos el archivo
+foreach ($sql as $sql2) {
+	if($connection->query($sql2)){
+		echo 'Tabla TYPE OF ACADEMIC INSTITUION creada <br/> ';
+		break;
+	}else{
+		echo $connection->error;
+		break;
+	}	
+}
+//LLENAMOS LA TABLA TIPO DE INSTITUCIÓN ACADEMICA
+$sql = explode(";", file_get_contents('data_base/fill_type_of_academic_institution.sql')); //abrimos el archivo
+foreach ($sql as $sql2) {//recorremos todas las consultas
+	if(!empty($sql2)){
+		if($connection->query($sql2)){
+			echo 'fila afectada <br/>';		
+		}else{
+			break;
+		}	
+	} else {
+		break;
+	}
+}
+
+//CREAMOS LA TABLA INSTITUCIÓN ACADEMICA
+$sql = explode(";", file_get_contents('data_base/create_academic_institution.sql')); //abrimos el archivo
+foreach ($sql as $sql2) {
+	if($connection->query($sql2)){
+		echo 'Tabla INSTITUCIÓN ACADEMICA creada <br/> ';
+		break;
+	}else{
+		echo $connection->error;
+		break;
+	}	
+}
+
+//CREAMOS LA TABLA TIPO DE USUARIO
+$sql = explode(";", file_get_contents('data_base/create_type_of_user.sql')); //abrimos el archivo
+foreach ($sql as $sql2) {
+	if($connection->query($sql2)){
+		echo 'Tabla TIPO DE USUARIO creada <br/> ';
+		break;
+	}else{
+		echo $connection->error;
+		break;
+	}	
+}
+
+//LLENAMOS LA TABLA TIPO DE USUARIO
+$sql = explode(";", file_get_contents('data_base/fill_type_of_user.sql')); //abrimos el archivo
+foreach ($sql as $sql2) {//recorremos todas las consultas
+	if(!empty($sql2)){
+		if($connection->query($sql2)){
+			echo 'fila afectada <br/>';		
+		}else{
+			break;
+		}	
+	} else {
+		break;
+	}
+}
+
+//CREAMOS LA TABLA USUARIO
+$sql = explode(";", file_get_contents('data_base/create_user.sql')); //abrimos el archivo
+foreach ($sql as $sql2) {
+	if($connection->query($sql2)){
+		echo 'Tabla USUARIO creada <br/> ';
+		break;
+	}else{
+		echo $connection->error;
+		break;
+	}	
+}
+
+//CREAMOS LA TABLA AREA
+$sql = explode(";", file_get_contents('data_base/create_area.sql')); //abrimos el archivo
+foreach ($sql as $sql2) {
+	if($connection->query($sql2)){
+		echo 'Tabla AREA creada <br/> ';
+		break;
+	}else{
+		echo $connection->error;
+		break;
+	}	
+}
+
+//CREAMOS LA TABLA TIPO DE PUBLICATIÓN
+$sql = explode(";", file_get_contents('data_base/create_type_of_publication.sql')); //abrimos el archivo
+foreach ($sql as $sql2) {
+	if($connection->query($sql2)){
+		echo 'Tabla TIPO DE PUBLICACIÓN creada <br/> ';
+		break;
+	}else{
+		echo $connection->error;
+		break;
+	}	
+}
+
+//LLENAMOS LA TABLA TIPO DE PUBLICACIÓN
+$sql = explode(";", file_get_contents('data_base/fill_type_of_publication.sql')); //abrimos el archivo
+foreach ($sql as $sql2) {//recorremos todas las consultas
+	if(!empty($sql2)){
+		if($connection->query($sql2)){
+			echo 'fila afectada <br/>';		
+		}else{
+			echo $connection->error;
+			break;
+		}	
+	} else {
+		break;
+	}
+}
+
+//CREAMOS LA TABLA PUBLICACIÓN
+$sql = explode(";", file_get_contents('data_base/create_publication.sql')); //abrimos el archivo
+foreach ($sql as $sql2) {
+	if($connection->query($sql2)){
+		echo 'Tabla PUBLICACIÓN creada <br/> ';
+		break;
+	}else{
+		echo $connection->error;
+		break;
+	}	
+}
+
+//CREAMOS LA TABLA ENROLL
+$sql = explode(";", file_get_contents('data_base/create_enroll.sql')); //abrimos el archivo
+foreach ($sql as $sql2) {
+	if($connection->query($sql2)){
+		echo 'Tabla ENROLL creada <br/> ';
+		break;
+	}else{
+		echo $connection->error;
+		break;
+	}	
+}
+
+//CREAMOS LA TABLA REGISTRO
+$sql = explode(";", file_get_contents('data_base/create_register.sql')); //abrimos el archivo
+foreach ($sql as $sql2) {
+	if($connection->query($sql2)){
+		echo 'Tabla REGISTRO creada <br/> ';
+		break;
+	}else{
+		echo $connection->error;
+		break;
+	}	
+}
+
+//CREAMOS LA TABLA ADMINISTRADOR
+$sql = explode(";", file_get_contents('data_base/create_administrator.sql')); //abrimos el archivo
+foreach ($sql as $sql2) {
+	if($connection->query($sql2)){
+		echo 'Tabla ADMINISTRADOR creada <br/> ';
+		break;
+	}else{
+		echo $connection->error;
+		break;
+	}	
+}
+
+//CERRANDO LA CONEXIÓN ESTABLECIDA
+mysqli_close($connection);
+?>
