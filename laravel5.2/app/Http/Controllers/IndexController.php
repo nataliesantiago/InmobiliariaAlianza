947be0c75bb735770485d62948d0bf7d27da48e7
nataliesantiago <naca_sato@hotmail.com>
2016-04-10 15:24:58 -0500

    Revert "creacion de la carpeta Models en la que se encuentran todos lo modelos del proyecto"
    This reverts commit edd2678d6aa801d6988eb333a5588843f5f3b6d9.

--- laravel5.2/app/Http/Controllers/IndexController.php
+++ laravel5.2/app/Http/Controllers/IndexController.php
@@ -6,12 +6,11 @@ use Illuminate\Http\Request;
 
 use MunDocente\Http\Requests;
 use MunDocente\Http\Controllers\Controller;
-use MunDocente\Models\Publication;
 
 class IndexController extends Controller
 {
     public function index(){
-    	$publications = Publication::all();
+    	$publications = \MunDocente\Publication::all();
     	return view('index', compact('publications'));
     }
 }
