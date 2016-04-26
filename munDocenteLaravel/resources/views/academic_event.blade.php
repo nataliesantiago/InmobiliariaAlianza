14403406beeda5d9bce5020dc6e382fbb310b454
nataliesantiago <naca_sato@hotmail.com>
2016-04-26 14:59:13 -0500

    modificacion de las vistas (se incluyo el login) para dejar las plantillas con solo el header y el footer

--- munDocenteLaravel/resources/views/academic_event.blade.php
+++ munDocenteLaravel/resources/views/academic_event.blade.php
@@ -1,4 +1,51 @@
 @extends('layouts.info_publication')
+
+@section('login')
+ @if (Auth::guest())
+                <div class="4u 12u(mobile)">
+
+                <section class="box">
+                            <form class="form login-form" role="form" method="POST" action="{{ url('/login') }}">
+                             {!! csrf_field() !!}
+                                <div class="input-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>                 
+                                    <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="Usuario"/>
+                                     @if ($errors->has('username'))
+                                             <span class="help-block">
+                                                 <strong>{{ $errors->first('username') }}</strong>
+                                            </span>
+                                         @endif
+                                </div><br>
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>                 
+                                    <input type="password" class="form-control" name="password" placeholder="Contraseña" />
+                                    @if ($errors->has('password'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('password') }}</strong>
+                                            </span>
+                                        @endif
+                                </div>
+                                <center><label class="checkbox inline">
+                                </label><input type="checkbox" name="remember">  Recordar</center><br>
+                                <center>
+                                <button type="submit" class="btn2 btn-default">Ingresar</button>
+                                    <ul class="social_login">
+                                        
+                                        <li><a class="icon fa-facebook" href="#"><span class="label">Facebook</span></a></li>
+                                    <li><a class="icon fa-google-plus" href="#"><span class="label">Google+</span></a></li>
+                                    </ul>
+                                    
+                                </center>
+                                <HR>
+                                <center><a class="forgot btn-link" href="{{ url('/password/reset') }}">¿Olvido su contraseña?</a></center>
+                            </form>
+                        <hr >       
+                    </section>
+
+                </div>
+                @endif
+        @stop
+
 @section('content')
 
 
--- munDocenteLaravel/resources/views/layouts/app.blade.php
+++ munDocenteLaravel/resources/views/layouts/app.blade.php
@@ -115,49 +115,7 @@ Free for personal and commercial use under the CCA 3.0 license (html5up.net/lice
                 @yield('content')
                     </div>
                 </div>
-                 @if (Auth::guest())
-                <div class="4u 12u(mobile)">
-
-                <section class="box">
-                            <form class="form login-form" role="form" method="POST" action="{{ url('/login') }}">
-                             {!! csrf_field() !!}
-                                <div class="input-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>                 
-                                    <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="Usuario"/>
-                                     @if ($errors->has('username'))
-                                             <span class="help-block">
-                                                 <strong>{{ $errors->first('username') }}</strong>
-                                            </span>
-                                         @endif
-                                </div><br>
-                                <div class="input-group">
-                                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>                 
-                                    <input type="password" class="form-control" name="password" placeholder="Contraseña" />
-                                    @if ($errors->has('password'))
-                                            <span class="help-block">
-                                                <strong>{{ $errors->first('password') }}</strong>
-                                            </span>
-                                        @endif
-                                </div>
-                                <center><label class="checkbox inline">
-                                </label><input type="checkbox" name="remember">Recordar</center><br>
-                                <center>
-                                <button type="submit" class="btn2 btn-default">Ingresar</button>
-                                    <ul class="social_login">
-                                        
-                                        <li><a class="icon fa-facebook" href="#"><span class="label">Facebook</span></a></li>
-                                    <li><a class="icon fa-google-plus" href="#"><span class="label">Google+</span></a></li>
-                                    </ul>
-                                    
-                                </center>
-                                <HR>
-                                <center><a class="forgot btn-link" href="{{ url('/password/reset') }}">¿Olvido su contraseña?</a></center>
-                            </form>
-                        <hr >       
-                    </section>
-
-                </div>
-                @endif
+               @yield('login')
 
             </div>
         </div>
