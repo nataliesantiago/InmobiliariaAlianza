4ae1668dde7282d7514ec510dae0e43f51643079
Diego Sierra <diego.sierra01@uptc.edu.co>
2016-07-24 13:33:29 -0500

    Indicadores para controlar el carrusel

--- munDocenteLaravel/public/js/slider.js
+++ munDocenteLaravel/public/js/slider.js
@@ -1,7 +1,37 @@
-$(function(){
-	$('#slider div:gt(0)').hide();
-	setInterval(function(){
-		$('#slider div:first-child').fadeOut(0)
-		.next('div').fadeIn(2000)
-		.end().appendTo('#slider');}, 5000);
+$(document).ready(function(){
+	//$('#slider div:first-child').fadeOut(0)
+	//	.next('div').fadeIn(2000)
+	//	.end().appendTo('#slider');}, 5000);
+    // Activate Carousel
+    $("#slider").carousel({
+    	interval: 4000
+    });
+    
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#slider").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#slider").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#slider").carousel(2);
+    });
+    $(".item4").click(function(){
+        $("#slider").carousel(3);
+    });
+    $(".item5").click(function(){
+        $("#slider").carousel(4);
+    });
+    $(".item6").click(function(){
+        $("#slider").carousel(5);
+    });
+    
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#slider").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#slider").carousel("next");
+    });
 });
--- munDocenteLaravel/resources/views/main.blade.php
+++ munDocenteLaravel/resources/views/main.blade.php
@@ -12,33 +12,56 @@
         <div class="container">
             <div class="row">
             
-            @if (Auth::guest())
-             <div class="4u 12u(mobile)">    
+             <div class="4u 12u(mobile)">
+             @if (Auth::guest())     
                @yield('login')
                @yield('area')
+             @else
+               @yield('area')
+             @endif
                </div>
                
                 <div class="8u 12u(mobile)">
                     <article class="box post">
-                         <div id = "slider">
-                            <div class ="elemento">
-                                <img src="images/img1.png">
+                         <div id="slider" class="carousel slide" >
+                            <!-- Indicators -->
+                            <ol class="carousel-indicators">
+                                <li class="item1 active"></li>
+                                <li class="item2"></li>
+                                <li class="item3"></li>
+                                <li class="item4"></li>
+                                <li class="item5"></li>
+                                <li class="item6"></li>
+                            </ol>
+                            <div class="carousel-inner" role="listbox">
+                                <div class ="item active">
+                                    <img src="images/img1.png">
+                                </div>
+                                <div class ="item">
+                                    <img src="images/img3.png">
+                                </div>
+                                 <div class ="item">
+                                    <img src="images/img4.png">
+                                </div>
+                                <div class ="item">
+                                    <img src="images/img2.png">
+                                </div>
+                                <div class ="item">
+                                    <img src="images/img6.png">
+                                </div>
+                                <div class ="item">
+                                    <img src="images/img7.png">
+                                </div>
                             </div>
-                            <div class ="elemento">
-                                <img src="images/img2.png">
-                            </div>
-                            <div class ="elemento">
-                                <img src="images/img3.png">
-                            </div>
-                             <div class ="elemento">
-                                <img src="images/img4.png">
-                            </div>
-                            <div class ="elemento">
-                                <img src="images/img6.png">
-                            </div>
-                            <div class ="elemento">
-                                <img src="images/img7.png">
-                            </div>
+
+                             <a class="left carousel-control" href="#slider" role="button">
+                              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                              <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="right carousel-control" href="#slider" role="button">
+                              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                              <span class="sr-only">Next</span>
+                            </a>
                         </div>
                         <br>
 
@@ -47,44 +70,6 @@
                     </article>
       
                 </div>
-              
-            @else   
-            <div class="4u 12u(mobile)">    
-               
-               @yield('area')
-               </div>
-
-                <div class="8u 12u(mobile)">
-                    <article class="box post">
-                         <div id = "slider">
-                            <div class ="elemento">
-                                <img src="images/img1.png">
-                            </div>
-                            <div class ="elemento">
-                                <img src="images/img2.png">
-                            </div>
-                            <div class ="elemento">
-                                <img src="images/img3.png">
-                            </div>
-                             <div class ="elemento">
-                                <img src="images/img4.png">
-                            </div>
-                            <div class ="elemento">
-                                <img src="images/img6.png">
-                            </div>
-                              <div class ="elemento">
-                                <img src="images/img7.png">
-                            </div>
-                        </div>
-                        @yield('content')
-                    </article>
-
-
-                </div>
-
-            @endif
-
-
             </div>
         </div>
     </div>
